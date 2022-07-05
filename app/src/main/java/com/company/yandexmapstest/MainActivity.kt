374c34bd979d19709d47beb80f97ba3c1a874a2f
ga1duk <gaidukdg@yandex.ru>
2022-07-05 12:59:53 +0300

    yandex maps were integrated into project

--- app/src/main/java/com/company/yandexmapstest/MainActivity.kt
+++ app/src/main/java/com/company/yandexmapstest/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.company.yandexmapstest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.mapview.MapView
+
+
+private const val TARGET_LOCATION_LAT = 55.751574
+private const val TARGET_LOCATION_LON = 37.573856
+
+private lateinit var mapview: MapView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        MapKitFactory.setApiKey(MAPKIT_API_KEY)
+        MapKitFactory.initialize(this)
+
         setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+
+        mapview = findViewById(R.id.mapview)
+        mapview.map.move(
+            CameraPosition(Point(TARGET_LOCATION_LAT, TARGET_LOCATION_LON), 11F, 0F, 0F),
+            Animation(Animation.Type.SMOOTH, 0F),
+            null
+        )
+    }
+
+    override fun onStop() {
+        mapview.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        mapview.onStart()
     }
 }
